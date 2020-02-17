c47a17756effd1571203b248019aff80b7c61796
minhtung99 <tung07011999@gmail.com>
2020-02-17 22:15:32 +0700

    login screen

---
+++ app/src/main/java/com/example/titshop/ui/login/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.titshop.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.titshop.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
