f71dddf088fb244750370263c7d344cedf24df79
minhtung99 <tung07011999@gmail.com>
2020-02-18 11:13:46 +0700

    sửa màn screenLogin

--- app/src/main/java/com/example/titshop/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/titshop/ui/login/LoginActivity.java
@@ -1,16 +1,64 @@
 package com.example.titshop.ui.login;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
+import android.view.View;
 
 import com.example.titshop.R;
+import com.example.titshop.base.BaseActivity;
+import com.example.titshop.databinding.ActivityLoginBinding;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends BaseActivity<ActivityLoginBinding, LoginViewModel> {
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+    public Class<LoginViewModel> getViewmodel() {
+        return LoginViewModel.class;
+    }
+
+    @Override
+    public int getLayoutID() {
+        return R.layout.activity_login;
+    }
+
+    @Override
+    public void setBindingViewmodel() {
+        binding.setLogin(viewmodel);
+        event();
+    }
+
+    private void event() {
+        binding.btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewmodel.facebookLogin(LoginActivity.this);
+            }
+        });
+
+        binding.txtForgot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewmodel.forgot(LoginActivity.this);
+            }
+        });
+
+        binding.txtSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewmodel.signUp(LoginActivity.this);
+            }
+        });
+
+        binding.layoutFacebookLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewmodel.facebookLogin(LoginActivity.this);
+            }
+        });
+
+        binding.layoutSkypeLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewmodel.skypeLogin(LoginActivity.this);
+            }
+        });
     }
 }
