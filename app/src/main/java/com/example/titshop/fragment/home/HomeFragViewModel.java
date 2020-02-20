9b4f146fce06eab9c42480b50d70b2d22ed17c38
Sontitxxxx <sonkoii1995@gmail.com>
2020-02-20 14:53:21 +0700

    add fragment home

---
+++ app/src/main/java/com/example/titshop/fragment/home/HomeFragViewModel.java
@@ -0,0 +1,54 @@
+package com.example.titshop.fragment.home;
+
+import androidx.lifecycle.MutableLiveData;
+
+import com.example.titshop.adapter.CollectionAdapter;
+import com.example.titshop.adapter.TopTrendAdapter;
+import com.example.titshop.base.BaseViewmodel;
+import com.example.titshop.model.Product;
+import com.example.titshop.model.SliderItem;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class HomeFragViewModel extends BaseViewmodel {
+     CollectionAdapter CollectionAdapter = new CollectionAdapter();
+     TopTrendAdapter ToptrendAdapter = new TopTrendAdapter();
+     private MutableLiveData<List<SliderItem>> arrSildeItem = new MutableLiveData<>();
+     private MutableLiveData<ArrayList<Product>> arrProductCollection = new MutableLiveData<>();
+     private MutableLiveData<ArrayList<Product>> arrProductToptrend = new MutableLiveData<>();
+     public MutableLiveData<List<SliderItem>> getArrItem(){
+         List<SliderItem> arr = new ArrayList<>();
+         arr.add(new SliderItem("Đang giảm giá nè . Mua đi !!!","https://khoedep24gio.com/wp-content/uploads/2019/06/shop-ban-ao-doi-dep-o-ha-noi-baduy-shop-2.png"));
+         arr.add(new SliderItem("Đang giảm giá nè . Mua đi !!!","https://bizweb.dktcdn.net/100/104/183/products/quan-tay-baggy-ngan-hong.png?v=1470410349437"));
+         arr.add(new SliderItem("Đang giảm giá nè . Mua đi !!!","https://r8zlusvr.rocketcdn.com/templates/selectfashion.co.uk.new/assets/images/home/main-page-pods/pod1a-coats111.jpg"));
+         arr.add(new SliderItem("Đang giảm giá nè . Mua đi !!!","https://iqueenfashion.vn/uploads/98/637081193496541818-DSC09555.jpg"));
+         arrSildeItem.postValue(arr);
+         return arrSildeItem;
+     }
+     public MutableLiveData<ArrayList<Product>> getArrCollection(){
+         ArrayList<Product> arr = new ArrayList<>();
+         arr.add(new Product(" Korean Style Women","https://my-test-11.slatic.net/p/7/kobwa-korean-style-women-fashion-casual-pu-leather-ladies-handbag-shoulder-bucket-tote-bag-brown-8138-099175611-084e0414bbec596b0199d890d826bd3b-.jpg_600x600q80.jpg","$69","x","dress","99",4.5f));
+         arr.add(new Product("Bat sloweet wolan","https://ae01.alicdn.com/kf/HTB1DP_RXWSs3KVjSZPiq6AsiVXa5/2020-Plus-Size-Bat-Sleeved-Woolen-Coat-Scarf-Collar-Jackets-Women-Winter-Fashion-Outerwear-Thicker-Loose.jpg","$24.47","x","dress","132",4.5f));
+         arr.add(new Product("T-shrit ladies","https://i.pinimg.com/736x/fc/07/d4/fc07d472f7a97762b5e2374446a8543f.jpg","$24.89","x","x","23",4.5f));
+         arr.add(new Product("Big Deal Kevin","https://ae01.alicdn.com/kf/HTB1MftjbdzvK1RkSnfoq6zMwVXan/KENVY-Brand-Fashion-Women-s-High-end-Luxury-Winter-Contrast-Color-Jacquard-Knitted-Cotton-Casual-Sweater.jpg","$11.89","x","x","23",4.5f));
+         arr.add(new Product("Girl sweammer","https://d2nikgq6rubtj8.cloudfront.net/web/2019/Sept/04/nav/womens-nav.jpg","$12.94","x","x","56",4.5f));
+         arr.add(new Product("Sustainable Women's Fashion","https://ae01.alicdn.com/kf/HTB1YYlaPpXXXXX.XXXXq6xXFXXXx/Wanita-Piyama-Sutra-Celana-Pendek-Tombol-Lengan-Pendek-Kemeja-Dan-Pendek-Set-2016-Musim-Panas-Wanita.jpg","$99","x","x","96",4.5f));
+         arrProductCollection.postValue(arr);
+         return arrProductCollection;
+     }
+    public MutableLiveData<ArrayList<Product>> getArrToptrend(){
+        ArrayList<Product> arr = new ArrayList<>();
+        arr.add(new Product(" Korean Style Women","https://my-test-11.slatic.net/p/7/kobwa-korean-style-women-fashion-casual-pu-leather-ladies-handbag-shoulder-bucket-tote-bag-brown-8138-099175611-084e0414bbec596b0199d890d826bd3b-.jpg_600x600q80.jpg","$69","x","dress","99",4.5f));
+        arr.add(new Product("Bat sloweet wolan","https://ae01.alicdn.com/kf/HTB1DP_RXWSs3KVjSZPiq6AsiVXa5/2020-Plus-Size-Bat-Sleeved-Woolen-Coat-Scarf-Collar-Jackets-Women-Winter-Fashion-Outerwear-Thicker-Loose.jpg","$24.47","x","dress","132",4.5f));
+        arr.add(new Product("T-shrit ladies","https://i.pinimg.com/736x/fc/07/d4/fc07d472f7a97762b5e2374446a8543f.jpg","$24.89","x","x","23",4.5f));
+        arr.add(new Product("Big Deal Kevin","https://ae01.alicdn.com/kf/HTB1MftjbdzvK1RkSnfoq6zMwVXan/KENVY-Brand-Fashion-Women-s-High-end-Luxury-Winter-Contrast-Color-Jacquard-Knitted-Cotton-Casual-Sweater.jpg","$11.89","x","x","23",4.5f));
+        arr.add(new Product("Girl sweammer","https://d2nikgq6rubtj8.cloudfront.net/web/2019/Sept/04/nav/womens-nav.jpg","$12.94","x","x","56",4.5f));
+        arr.add(new Product("Sustainable Women's Fashion","https://ae01.alicdn.com/kf/HTB1YYlaPpXXXXX.XXXXq6xXFXXXx/Wanita-Piyama-Sutra-Celana-Pendek-Tombol-Lengan-Pendek-Kemeja-Dan-Pendek-Set-2016-Musim-Panas-Wanita.jpg","$99","x","x","96",4.5f));
+        Collections.shuffle(arr);
+        arrProductToptrend.postValue(arr);
+        return arrProductToptrend;
+    }
+}
--- app/src/main/java/com/example/titshop/ui/home/HomeActivity.java
+++ app/src/main/java/com/example/titshop/ui/home/HomeActivity.java
@@ -12,12 +12,15 @@ import androidx.fragment.app.Fragment;
 
 import com.example.titshop.R;
 import com.example.titshop.base.BaseActivity;
+import com.example.titshop.base.BaseFragment;
+import com.example.titshop.callback.ActionbarListener;
 import com.example.titshop.databinding.ActivityHomeBinding;
+import com.example.titshop.fragment.home.HomeFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.zip.Inflater;
 
-public class HomeActivity extends BaseActivity<ActivityHomeBinding,HomeViewModel> {
+public class HomeActivity extends BaseActivity<ActivityHomeBinding,HomeViewModel> implements ActionbarListener {
     @Override
     public Class<HomeViewModel> getViewmodel() {
         return HomeViewModel.class;
@@ -33,13 +36,13 @@ public class HomeActivity extends BaseActivity<ActivityHomeBinding,HomeViewModel
        binding.setViewmodel(viewmodel);
        setuptToolbar();
        event();
+       // load default fragment
+        rePlaceFragment(new HomeFragment());
     }
 
     private void setuptToolbar() {
         setSupportActionBar(binding.actionBar.toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
-//        Drawable drawable = ContextCompat.getDrawable(getApplicationContext(),R.drawable.ic_more_horiz_black_24dp);
-//        binding.actionBar.toolbar.setOverflowIcon(drawable);
     }
 
     private void event() {
@@ -48,11 +51,10 @@ public class HomeActivity extends BaseActivity<ActivityHomeBinding,HomeViewModel
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 switch (menuItem.getItemId()){
                     case R.id.navigationShop:
-                        hiddenIconback();
-                        Toast.makeText(HomeActivity.this, "click shop", Toast.LENGTH_SHORT).show();
+                        hiddenMore();
+                        rePlaceFragment(new HomeFragment());
                         break;
                     case R.id.navigationExplore:
-                        hiddenIconback();
                         Toast.makeText(HomeActivity.this, "click Explore", Toast.LENGTH_SHORT).show();
                         break;
                     case R.id.navigationNotification:
@@ -73,10 +75,11 @@ public class HomeActivity extends BaseActivity<ActivityHomeBinding,HomeViewModel
         });
     }
 
-    private void hiddenIconback() {
-        binding.actionBar.ivIcon.setVisibility(View.GONE);
-        binding.actionBar.title.setVisibility(View.GONE);
+    private void hiddenMore() {
         binding.actionBar.toolbar.getMenu().findItem(R.id.menuMore).setVisible(false);
+    }
+    private void ShowMore() {
+        binding.actionBar.toolbar.getMenu().findItem(R.id.menuMore).setVisible(true);
     }
     private void showIconback() {
         binding.actionBar.ivIcon.setVisibility(View.VISIBLE);
@@ -89,6 +92,7 @@ public class HomeActivity extends BaseActivity<ActivityHomeBinding,HomeViewModel
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        hiddenMore();
         return true;
     }
 
@@ -107,5 +111,14 @@ public class HomeActivity extends BaseActivity<ActivityHomeBinding,HomeViewModel
             default:
                 return super.onContextItemSelected(item);
         }
+    }
+
+    @Override
+    public void onResumFragment(BaseFragment fragment) {
+        if(fragment instanceof HomeFragment){
+             binding.actionBar.ivIcon.setVisibility(View.GONE);
+             binding.actionBar.title.setText("Shopping");
+        }
+
     }
 }
