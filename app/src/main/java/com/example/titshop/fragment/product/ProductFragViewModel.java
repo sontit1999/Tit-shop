8e31ccd7d6223437c657bbc0ad142eec7d77c925
Sontitxxxx <sonkoii1995@gmail.com>
2020-02-21 13:52:28 +0700

    fix responsive slide multi device

--- app/src/main/java/com/example/titshop/fragment/product/ProductFragViewModel.java
+++ app/src/main/java/com/example/titshop/fragment/product/ProductFragViewModel.java
@@ -21,6 +21,18 @@ public class ProductFragViewModel extends BaseViewmodel {
         arr.add(new SubProduct("Big Deal Kevin", "https://ae01.alicdn.com/kf/HTB1MftjbdzvK1RkSnfoq6zMwVXan/KENVY-Brand-Fashion-Women-s-High-end-Luxury-Winter-Contrast-Color-Jacquard-Knitted-Cotton-Casual-Sweater.jpg", "$11.89", "x", "x", true, 4.5f));
         arr.add(new SubProduct("Girl sweammer", "https://d2nikgq6rubtj8.cloudfront.net/web/2019/Sept/04/nav/womens-nav.jpg", "$12.94", "x", "x", true, 4.5f));
         arr.add(new SubProduct("Sustainable Women's Fashion", "https://ae01.alicdn.com/kf/HTB1YYlaPpXXXXX.XXXXq6xXFXXXx/Wanita-Piyama-Sutra-Celana-Pendek-Tombol-Lengan-Pendek-Kemeja-Dan-Pendek-Set-2016-Musim-Panas-Wanita.jpg", "$99", "x", "x", true, 4.5f));
+        arr.add(new SubProduct(" Korean Style Women", "https://my-test-11.slatic.net/p/7/kobwa-korean-style-women-fashion-casual-pu-leather-ladies-handbag-shoulder-bucket-tote-bag-brown-8138-099175611-084e0414bbec596b0199d890d826bd3b-.jpg_600x600q80.jpg", "$69", "x", "dress", true, 4.5f));
+        arr.add(new SubProduct("Bat sloweet wolan", "https://ae01.alicdn.com/kf/HTB1DP_RXWSs3KVjSZPiq6AsiVXa5/2020-Plus-Size-Bat-Sleeved-Woolen-Coat-Scarf-Collar-Jackets-Women-Winter-Fashion-Outerwear-Thicker-Loose.jpg", "$24.47", "x", "dress", true, 4.5f));
+        arr.add(new SubProduct("T-shrit ladies", "https://i.pinimg.com/736x/fc/07/d4/fc07d472f7a97762b5e2374446a8543f.jpg", "$24.89", "x", "x", true, 4.5f));
+        arr.add(new SubProduct("Big Deal Kevin", "https://ae01.alicdn.com/kf/HTB1MftjbdzvK1RkSnfoq6zMwVXan/KENVY-Brand-Fashion-Women-s-High-end-Luxury-Winter-Contrast-Color-Jacquard-Knitted-Cotton-Casual-Sweater.jpg", "$11.89", "x", "x", true, 4.5f));
+        arr.add(new SubProduct("Girl sweammer", "https://d2nikgq6rubtj8.cloudfront.net/web/2019/Sept/04/nav/womens-nav.jpg", "$12.94", "x", "x", true, 4.5f));
+        arr.add(new SubProduct("Sustainable Women's Fashion", "https://ae01.alicdn.com/kf/HTB1YYlaPpXXXXX.XXXXq6xXFXXXx/Wanita-Piyama-Sutra-Celana-Pendek-Tombol-Lengan-Pendek-Kemeja-Dan-Pendek-Set-2016-Musim-Panas-Wanita.jpg", "$99", "x", "x", true, 4.5f));
+        arr.add(new SubProduct(" Korean Style Women", "https://my-test-11.slatic.net/p/7/kobwa-korean-style-women-fashion-casual-pu-leather-ladies-handbag-shoulder-bucket-tote-bag-brown-8138-099175611-084e0414bbec596b0199d890d826bd3b-.jpg_600x600q80.jpg", "$69", "x", "dress", true, 4.5f));
+        arr.add(new SubProduct("Bat sloweet wolan", "https://ae01.alicdn.com/kf/HTB1DP_RXWSs3KVjSZPiq6AsiVXa5/2020-Plus-Size-Bat-Sleeved-Woolen-Coat-Scarf-Collar-Jackets-Women-Winter-Fashion-Outerwear-Thicker-Loose.jpg", "$24.47", "x", "dress", true, 4.5f));
+        arr.add(new SubProduct("T-shrit ladies", "https://i.pinimg.com/736x/fc/07/d4/fc07d472f7a97762b5e2374446a8543f.jpg", "$24.89", "x", "x", true, 4.5f));
+        arr.add(new SubProduct("Big Deal Kevin", "https://ae01.alicdn.com/kf/HTB1MftjbdzvK1RkSnfoq6zMwVXan/KENVY-Brand-Fashion-Women-s-High-end-Luxury-Winter-Contrast-Color-Jacquard-Knitted-Cotton-Casual-Sweater.jpg", "$11.89", "x", "x", true, 4.5f));
+        arr.add(new SubProduct("Girl sweammer", "https://d2nikgq6rubtj8.cloudfront.net/web/2019/Sept/04/nav/womens-nav.jpg", "$12.94", "x", "x", true, 4.5f));
+        arr.add(new SubProduct("Sustainable Women's Fashion", "https://ae01.alicdn.com/kf/HTB1YYlaPpXXXXX.XXXXq6xXFXXXx/Wanita-Piyama-Sutra-Celana-Pendek-Tombol-Lengan-Pendek-Kemeja-Dan-Pendek-Set-2016-Musim-Panas-Wanita.jpg", "$99", "x", "x", true, 4.5f));
         arrSubProductCollection.postValue(arr);
         return arrSubProductCollection;
     }
